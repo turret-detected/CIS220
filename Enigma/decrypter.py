2d8af9c5ea2699725f82dbea3f696f4587671365
Andrew Mohnkern <andrew.mohnkern@gmail.com>
2019-06-19 19:44:20 -0400

    Comment additions 1

--- Enigma/decrypter.py
+++ Enigma/decrypter.py
@@ -5,8 +5,12 @@
 #Filename: decrypter.py
 #Desc: Provides inverted dictionary of plugboard
 
-def invertedDict():
+def invertedDict(): #returns the inverted version of the plugboard
     import plugboard
     invert = plugboard.getPlugboard()
     inv_dict = {v: k for k, v in invert.items()}
     return inv_dict
+
+#TODO
+#The function should take a dictionary as an argument
+#However it's default should be the plugboard dictionary
