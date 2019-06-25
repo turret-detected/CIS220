73ff7d4302ae12d572e9cda30560113db68a260d
Zachary Page <51968089+ZachsFinalForm@users.noreply.github.com>
2019-06-25 13:52:34 -0400

    Update decrypter.py

--- Enigma/decrypter.py
+++ Enigma/decrypter.py
@@ -4,10 +4,9 @@
 #Project: Enigma Encryptor
 #Filename: decrypter.py
 #Desc: Provides inverted dictionary of plugboard
-
-def invertedDict(): #returns the inverted version of the plugboard
-    import plugboard
-    invert = plugboard.getPlugboard()
+import plugboard
+defaultDict = plugboard.getPlugboard()
+def invertedDict(invert = defaultDict): #returns the inverted version of the plugboard
     inv_dict = {v: k for k, v in invert.items()}
     return inv_dict
 
