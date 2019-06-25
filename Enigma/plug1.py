7ee8eaa28cd4fa26eda0cded453979c48fa8d2d4
Andrew M <andrew.mohnkern@gmail.com>
2019-06-25 16:17:18 -0400

    fix count bug

--- Enigma/plug1.py
+++ Enigma/plug1.py
@@ -10,26 +10,28 @@ plug = plugboard.getPlugboard()
 #plug.update({" ":" ","\n":"\n"}) #temporary until character validation is added
    
 def translate(phrase,dictionary=plug): #each string char is a key from dict, outputs value
-    count = 0
-    translation = ""
-    for char in phrase:
-        if char in dictionary.values():
-                translation += dictionary[char]   
-        else:
-            if char == " ":
-                ++count
-                translation  += " "
-            elif char ==  "\n":
-                    pass
-            else:
-                   translation += char 
-        if count >= 5:
-            count = 0
-            translation += "\n" 
-        
-        
-    #print(translation)     
-    return translation
+	count = 0
+	translation = ""
+	for char in phrase:
+		if count == 5:
+			count = 0
+			#print("adding newline") #DEBUG
+			translation += "\n" 
+		
+		if char in dictionary.values():
+			translation += dictionary[char]	  
+		else:
+			if char == " ":
+				count = count + 1
+				#print("Adding count" + str(count)) #DEBUG
+				translation	 += " "
+			elif char ==  "\n":
+				pass
+			else:
+				translation += char 
+		
+	#print(translation)	#DEBUG	
+	return translation
 
 #TODO	
 #add to for loop
