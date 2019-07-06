699fdaf3ddb96f57ca05c85ca1a84d3fdbed8468
turret-detected <andrew.mohnkern@gmail.com>
2019-07-06 01:34:14 -0400

    error messages for bad file selection

--- Enigma/gui.py
+++ Enigma/gui.py
@@ -12,23 +12,46 @@ def run_gui():
 		
 		InFilePath = ""
 		OutFilePath = ""
-			
-		def areNull():
+		
+		def checkInput(): #returns false if input filepath is invalid, throws appropriate errorboxes
+			InExt = FilePaths.InFilePath[-4:] #takes last four characters
+			#print(InExt) #DEBUG
+		
 			if FilePaths.InFilePath == "":
-				return True
-			elif FilePaths.OutFilePath == "":
-				return True
-			else:
+				appMain.errorBox("Error", "You have not selected a file to encrypt.")
 				return False
-
-
+			
+			elif InExt != ".txt":
+				appMain.errorBox("Error", "Your input file must be a text file.")
+				FilePaths.InFilePath = ""
+				return False
+				
+			else:
+				return True
+				
+		def checkOutput(): #returns false if output filepath is invalid, throws appropriate errorBoxes
+			OutExt = FilePaths.OutFilePath[-4:] #takes last four characters
+			
+			if FilePaths.OutFilePath == "":
+				appMain.errorBox("Error", "You have not selected a file to output to.")
+				return False
+			
+			elif OutExt != ".txt":
+				appMain.errorBox("Error", "Your output file must be a text file.")
+				FilePaths.OutFilePath = ""
+				return False
+				
+			else:
+				return True
+			
+			
 	#Main window
 	appMain = gui("Enigma", "800x600")
 	appMain.setBg("cyan")
 	
 	
 	#TOP
-	appMain.startFrame("TOP", row=0, column=0, colspan=2)
+	appMain.startFrame("TOP", row=0, column=0, colspan=2) #centered top frame
 	
 	appMain.addLabel("title", "Enigma")
 	appMain.setLabelBg("title", "cyan")
@@ -56,16 +79,20 @@ def run_gui():
 	#MIDDLE RIGHT
 	appMain.startFrame("MIDDLE_RIGHT", row=1, column=1)
 	
-	def selectIn():
+	def selectIn(): #file selection in
 		FilePaths.InFilePath = utilities.getTextFilePath()
+		null = FilePaths.checkInput()
 		appMain.setLabel("InputFile", FilePaths.InFilePath)
 	
-	def selectOut():
+	def selectOut(): #file selection out
 		FilePaths.OutFilePath = utilities.getSaveFilePath()
+		null = FilePaths.checkOutput()
 		appMain.setLabel("OutputFile", FilePaths.OutFilePath)
 	
 	appMain.addButton("SelectIn", selectIn)
 	appMain.addButton("SelectOut", selectOut)
+	appMain.setButton("SelectIn", "Choose input file")
+	appMain.setButton("SelectOut", "Choose output file")
 	#appMain.setButtonPadding("SelectIn", [100, 0])
 	#appMain.setButtonPadding("SelectOut", [100, 0])
 	#appMain.setButtonAlign("SelectIn", "right")
@@ -78,23 +105,22 @@ def run_gui():
 	appMain.stopFrame()
 	
 	#BOTTOM
-	appMain.startFrame("BOTTOM", row=2, column=0, colspan=2)
+	appMain.startFrame("BOTTOM", row=2, column=0, colspan=2) #centered bottom frame
 	
 	def encryptButton():
-		if not FilePaths.areNull():
-			utilities.encryptTextArgs(FilePaths.InFilePath, FilePaths.OutFilePath)
-		else:
-			print("ERROR")
-
+		if FilePaths.checkInput():
+			if FilePaths.checkOutput(): #check in and out, just in case
+				utilities.encryptTextArgs(FilePaths.InFilePath, FilePaths.OutFilePath)
+				appMain.infoBox("Done", "Encryption complete")
+			
 	def decryptButton():
-		if not FilePaths.areNull():
-			utilities.decryptTextArgs(FilePaths.InFilePath, FilePaths.OutFilePath)
-		else:
-			print("ERROR")
-	
-	
+		if FilePaths.checkInput():
+			if FilePaths.checkOutput(): #check in and out, just in case
+				utilities.decryptTextArgs(FilePaths.InFilePath, FilePaths.OutFilePath)
+				appMain.infoBox("Done", "Decryption complete")
+
+
 	def mainButtons(button):
-	
 		#Encrypt
 		if button == "Encrypt":
 			encryptButton()
